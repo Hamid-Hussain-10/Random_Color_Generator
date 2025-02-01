48eeb86a215bfffac9c6e771eeb7a1ebf5e0f08c
Hamid Hussain <hhdev456@gmail.com>
2025-02-01 19:58:24 +0500

    new

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// let my_bio = {
-//   name: "Hamid Hussain",
-//   phone_no: 3342354326,
-//   marks: 95,
-// };
-
-// console.log(my_bio);
+let my_bio = {
+  name: "Hamid Hussain",
+  phone_no: 3342354326,
+  marks: 95,
+};
+console.log(my_bio);
 
 let boxes = document.querySelector(".container").children;
 
@@ -12,11 +11,14 @@ function getRandomColor() {
   let value1 = Math.floor(Math.random() * 250);
   let value2 = Math.floor(Math.random() * 250);
   let value3 = Math.floor(Math.random() * 250);
-  return `rgb(${value1}, ${value2}, ${value3})`; // Corrected RGB format
+  return `rgb(${value1}, ${value2}, ${value3})`;
 }
 
-Array.from(boxes).forEach(e => {
+Array.from(boxes).forEach((e) => {
   e.style.backgroundColor = getRandomColor();
   e.style.color = getRandomColor();
 });
 
+function changeText() {
+  document.getElementById("demo").innerText = "Hello, Guuuys!";
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let my_bio = {
+  name: "Hamid Hussain",
+  phone_no: 3342354326,
+  marks: 95,
+};
+
+console.log(my_bio);
