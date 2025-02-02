2304253d61593405c5982a5268717bec0e63abe9
Hamid Hussain <hhdev456@gmail.com>
2025-02-02 10:51:18 +0500

    new

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ console.log(my_bio);
 let boxes = document.querySelector(".container").children;
 
 function getRandomColor() {
-  let value1 = Math.floor(Math.random() * 250);
-  let value2 = Math.floor(Math.random() * 250);
-  let value3 = Math.floor(Math.random() * 250);
+  let value1 = Math.floor(Math.random() * 256);
+  let value2 = Math.floor(Math.random() * 256);
+  let value3 = Math.floor(Math.random() * 256);
   return `rgb(${value1}, ${value2}, ${value3})`;
 }
 
-Array.from(boxes).forEach((e) => {
-  e.style.backgroundColor = getRandomColor();
-  e.style.color = getRandomColor();
-});
+function changeColors() {
+  Array.from(boxes).forEach((box) => {
+    box.style.backgroundColor = getRandomColor();
+    box.style.color = getRandomColor();
+  });
+}
+
+setInterval(changeColors, 3000);
 
 function changeText() {
   document.getElementById("demo").innerText = "Hello, Guuuys!";
